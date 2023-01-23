83d098b94dc484928c8c0f40aa371d87aa65ea19
Adrian Cieplucha <adik.cieplucha@gmail.com>
2023-01-23 21:04:14 +0100

    revert url

--- src/services/api/axios.ts
+++ src/services/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://english-educational-app-backend.vercel.ap",
+  baseURL: "https://english-educational-app-backend.vercel.app/api",
   withCredentials: true,
 });
