01a183d6e85d9a8f5e95e0fbf696165b39fd17e4
Adrian Cieplucha <adik.cieplucha@gmail.com>
2023-01-23 20:00:25 +0100

    baseURL change

--- src/services/api/axios.ts
+++ src/services/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://english-educational-app-backend.vercel.app/api",
   withCredentials: true,
 });
