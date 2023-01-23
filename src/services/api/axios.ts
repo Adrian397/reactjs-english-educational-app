8b4a5acc7d0e008e1de2b3291c3ffc8d1cae67c0
Adrian Cieplucha <adik.cieplucha@gmail.com>
2023-01-23 23:15:36 +0100

    add vercel json file

--- src/services/api/axios.ts
+++ src/services/api/axios.ts
@@ -4,3 +4,8 @@ export default axios.create({
   baseURL: "https://english-educational-app-backend.vercel.app/api",
   withCredentials: true,
 });
+
+// export default axios.create({
+//   baseURL: "http://localhost:3000/api",
+//   withCredentials: true,
+// });
