01ed917f78c3ba6e3e41eede755939e6d3e02d4e
Adrian Cieplucha <adik.cieplucha@gmail.com>
2023-01-24 20:47:27 +0100

    delete headers

--- src/services/SessionService.ts
+++ src/services/SessionService.ts
@@ -20,19 +20,10 @@ type ResetPasswordArgs = {
 const sessionServiceDef = () => {
   const login = async (args: LoginArgs) => {
     try {
-      const response = await axios.post(
-        "/login",
-        {
-          username: args.username,
-          password: args.password,
-        },
-        {
-          headers: {
-            /*eslint-disable-next-line*/
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-        }
-      );
+      const response = await axios.post("/login", {
+        username: args.username,
+        password: args.password,
+      });
 
       const { data } = response;
       const { token } = data;
