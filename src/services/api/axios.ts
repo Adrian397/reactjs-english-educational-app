085567994aec45ee59bd66dc22a9d4c34ea63308
Adrian Cieplucha <adik.cieplucha@gmail.com>
2023-01-24 23:38:44 +0100

    revert vercel hosting

--- src/services/api/axios.ts
+++ src/services/api/axios.ts
@@ -1,14 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://english-educational-app-backend.onrender.com/api",
+  baseURL: "https://english-educational-app-backend.vercel.app/api",
   withCredentials: true,
 });
-
-// export default axios.create({
-//   baseURL: "https://english-educational-app-backend.vercel.app/api",
-//   withCredentials: true,
-// });
 
 // export default axios.create({
 //   baseURL: "http://localhost:3000/api",
